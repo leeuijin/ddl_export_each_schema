8392dcfc981e669a795b8a09e6d40be851bc63c2
Uijin Lee <geartec82@gmail.com>
2024-11-20 17:29:10 +0900

    Update backup_each_schema.sh

--- backup_each_schema.sh
+++ backup_each_schema.sh
@@ -4,16 +4,34 @@ export PGDATABASE=gpadmin
 
 #init
 
-mkdir -p ./pg_dumpall
+rm -rf ./all_backup
+mkdir -p ./all_backup/conf
+mkdir -p ./all_backup/env
 rm -rf *.sql
 rm -rf *.ori
-rm -rf ./pg_dumpall/*.sql
+rm -rf ./all_backup/*.*
+rm -rf ./all_backup/conf/*
+rm -rf ./all_backup/env/*
 
 #pg_dumpall
 
-pg_dumpall --globals-only > ./all_bk/role.sql
-pg_dumpall --schema-only  > ./all_bk/schema_all.sql
+pg_dumpall --globals-only > ./all_backup/role.sql
+pg_dumpall --schema-only  > ./all_backup/pg_dump_all.sql
 
+#postgresql.conf backup
+
+cp /data/master/gpseg-1/postgresql.conf  ./all_backup/conf/mst_postgresql.conf
+scp -r gpadmin@sdw1:/data1/primary/gpseg0/postgresql.conf ./all_backup/conf/seg_postgresql.conf
+cp /data/master/gpseg-1/pg_hba.conf  ./all_backup/conf/mst_pg_hba.conf
+
+# gpadmin env
+cp /home/gpadmin/.bashrc ./all_backup/env/bashrc
+cp /home/gpadmin/.bash_profile ./all_backup/env/bash_profile
+cp /home/gpadmin/.pgpass ./all_backup/env/pgpass
+
+#OS Kernel
+cp /etc/sysctl.conf ./all_backup/env/mst_sysctl.conf
+scp -r gpadmin@sdw1:/etc/sysctl.conf ./all_backup/env/seg_sysctl.conf
 
 #user_schema_check
 
@@ -21,14 +39,14 @@ SCHEMA_NAME=$(psql -U gpadmin -t -A -c "SELECT nspname FROM pg_catalog.pg_namesp
 
 #each user schema ddl export
 
-IFS=$'\n' 
+IFS=$'\n'
 for nspname in $SCHEMA_NAME; do
     if [ -n "$nspname" ]; then
 	pg_dump --schema=$nspname --schema-only  > $nspname.sql
 	#cp $nspname.sql $nspname.ori
 	cat $nspname.sql |grep -B 1 'PRIMARY KEY' > PK.sql
 	sed 'N;s/\n/ /' PK.sql> $nspname"_crt_idx".sql
-	cat $nspname.sql |grep 'CREATE INDEX' 		>> $nspname"_crt_idx".sql 
+	cat $nspname.sql |grep 'CREATE INDEX' 		>> $nspname"_crt_idx".sql
 	sed 's/CREATE INDEX/-- CREATE INDEX/g' $nspname.sql  > $nspname"_without_index".sql
 	rm PK.sql
     fi
