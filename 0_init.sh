1186fa1155aeac96365e5162d3c35606f72ff45a
Uijin Lee <geartec82@gmail.com>
2024-11-22 03:06:05 +0900

    Update 0_init.sh

--- 0_init.sh
+++ 0_init.sh
@@ -5,12 +5,14 @@ export PGDATABASE=gpadmin
 #init
 
 #rm -rf ./all_backup
+mkdir -p ./all_backup/log
 mkdir -p ./all_backup/1_conf
 mkdir -p ./all_backup/2_env
 mkdir -p ./all_backup/3_pg_dumpall
 mkdir -p ./all_backup/4_os
-rm -rf *.sql
-rm -rf *.ori
+#rm -rf *.sql
+#rm -rf *.ori
+#rm -rf ./all_backup/log/*.log
 #rm -rf ./all_backup/*.*
 #rm -rf ./all_backup/1_conf/*
 #rm -rf ./all_backup/2_env/*
