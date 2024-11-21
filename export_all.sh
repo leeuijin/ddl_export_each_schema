e6760d3351d98a956e64005cf198e7b16eae5755
Uijin Lee <geartec82@gmail.com>
2024-11-22 03:14:30 +0900

    Update export_all.sh

--- export_all.sh
+++ export_all.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+TODAY=$(date "+%Y%m%d%H%M")
 
 export PGDATABASE=gpadmin
-sh 0_init.sh
-sh 1_conf_backup.sh
-sh 2_profile_backup.sh
-sh 3_pg_dumpall.sh
-sh 4_sysctl_backup.sh
-sh 5_each_schema_ddl_backup.sh
-sh 6_each_database_schema_ddl_backup.sh
-sh 7_gen_ddl_gpdb_4_to_6.sh
+
+sh 0_init.sh > ./all_backup/log/0_init.log.$TODAY
+sh 1_conf_backup.sh > ./all_backup/log/1_conf_backup.log.$TODAY
+sh 2_profile_backup.sh > ./all_backup/log/2_profile_backup.log.$TODAY
+sh 3_pg_dumpall.sh > ./all_backup/log/3_pg_dumpall.log.$TODAY
+sh 4_sysctl_backup.sh > ./all_backup/log/4_sysctl_backup.log.$TODAY
+sh 5_each_schema_ddl_backup.sh > ./all_backup/log/5_each_schema_ddl_backup.log.$TODAY
+sh 6_each_database_schema_ddl_backup.sh > ./all_backup/log/6_each_database_schema_ddl_backup.log.$TODAY
+sh 7_gen_ddl_gpdb_4_to_6.sh > ./all_backup/log/7_gen_ddl_gpdb_4_to_6.log.$TODAY
