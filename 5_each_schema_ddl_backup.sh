3892f34b48b0382ea0e31d8cae64f7db8e3df8c1
Uijin Lee <geartec82@gmail.com>
2024-11-22 03:21:39 +0900

    Update 5_each_schema_ddl_backup.sh

--- 5_each_schema_ddl_backup.sh
+++ 5_each_schema_ddl_backup.sh
@@ -12,17 +12,17 @@ SCHEMA_NAME=$(psql -U gpadmin -t -A -c "SELECT nspname FROM pg_catalog.pg_namesp
 
 mkdir -p ./all_backup/5_ddl
 
-PWD=./all_backup/5_ddl 
+PWD=./all_backup/5_ddl
 
-IFS=$'\n' 
+IFS=$'\n'
 for nspname in $SCHEMA_NAME; do
     if [ -n "$nspname" ]; then
 	pg_dump --schema=$nspname --schema-only  > $PWD/$nspname.sql
 	cp $PWD/$nspname.sql $PWD/$nspname.ori
 	cat $PWD/$nspname.sql |grep -B 1 'PRIMARY KEY' > $PWD/PK.sql
 	sed 'N;s/\n/ /' $PWD/PK.sql> $PWD/$nspname"_crt_idx".sql
-	cat $PWD/$nspname.sql |grep 'CREATE INDEX' 		>> $PWD/$nspname"_crt_idx".sql 
-	cat $PWD/$nspname.sql |sed 's/CREATE INDEX/ -- CREATE INDEX/g' | sed 's/ALTER TABLE ONLY/-- ALTER TABLE ONLY/g' | sed 's/ADD CONSTRAINT/-- ADD CONSTRAINT/g' > $PWD/$nspname"_without_index".sql	
+	cat $PWD/$nspname.sql |grep 'CREATE INDEX' 		>> $PWD/$nspname"_crt_idx".sql
+	cat $PWD/$nspname.sql |sed 's/CREATE INDEX/ -- CREATE INDEX/g' | sed 's/ALTER TABLE ONLY/-- ALTER TABLE ONLY/g' | sed 's/ADD CONSTRAINT/-- ADD CONSTRAINT/g' > $PWD/$nspname"_without_index".sql
 	rm $PWD/PK.sql
     fi
 done
