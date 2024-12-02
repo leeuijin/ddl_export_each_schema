52fd3ea9f8ae7062a99fbc0380dc1edae0531bc4
Uijin Lee <geartec82@gmail.com>
2024-12-03 02:48:59 +0900

    Update 2_profile_backup.sh

--- 2_profile_backup.sh
+++ 2_profile_backup.sh
@@ -6,6 +6,7 @@ TODAY=$(date "+%Y%m%d")
 cp /home/gpadmin/.bashrc ./all_backup/2_env/bashrc_$TODAY
 cp /home/gpadmin/.bash_profile ./all_backup/2_env/bash_profile_$TODAY
 cp /home/gpadmin/.pgpass ./all_backup/2_env/pgpass_$TODAY
+cp /home/gpadmin/.profile ./all_backup/2_env/profile_$TODAY
 
 
 
